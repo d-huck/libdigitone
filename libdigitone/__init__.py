5fc8b203a45c2b27fbebea63c19965aec9b36f44
Davin Huckleberry <dhuck@tinycact.us>
2018-10-19 01:32:35 -0500

    scaffolding

--- libdigitone/__init__.py
+++ libdigitone/__init__.py
@@ -1,10 +1,2 @@
-from binascii import hexlify, unhexlify
-
-def parse():
+if __name__ == '__main__':
     pass
-
-    # TODO: Write Parse Function
-    #
-    # This function will need to take the input file of SEVERAL sysex messages and break them into their individual mes-
-    # sages. This will only create an array of individual messages, not sounds for forward compatibility as I build out
-    # more modules and support for projects/patterns.
