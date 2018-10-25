3c7b267e12facc0a42cdf0122aa00ada73f6b9bf
Davin Huckleberry <dhuck@tinycact.us>
2018-10-25 01:30:38 -0500

    beginning pydoc

--- libdigitone/__init__.py
+++ libdigitone/__init__.py
@@ -1,6 +1,12 @@
 from .sound import *
 from .sysex import *
 
+# TODO: Write proper docstring for the project
+"""
+libdigitone
 
+A portable python library for interfacing with the Elektron Digitone
+
+"""
 if __name__ == '__main__':
     pass
